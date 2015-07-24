3fb054691e55ae5ef2a3afd395a04a9c952ad42f
CodeFluent <wasfimomenwim@gmail.com>
2015-07-24 13:26:00 -0400

    Added new html, css, and js files to render markers and map

--- gsuMapJS.js
+++ gsuMapJS.js
@@ -1,58 +1,29 @@
-var mapOptions ={
-	center: new google.maps.LatLng(33.753475, -84.385301),
-	zoom:15,
-	mapTypeId: google.maps.MapTypeId.ROADMAP
-};
 
-// autocomplete type in destination and google finds the establishment for you
-var map = new google.maps.Map(document.getElementById('map'), mapOptions);
-var acOptions = {
-  types: ['establishment']
-};
-var autocomplete = new google.maps.places.Autocomplete(document.getElementById('autocomplete'),acOptions);
-autocomplete.bindTo('bounds',map);
-var infoWindow = new google.maps.InfoWindow();
-var marker = new google.maps.Marker({
+function initialize () {
+var mapOptions = {
+  center: new google.maps.LatLng(33.753475, -84.385301),
+  zoom: 16,
+  mapTypeId: google.maps.MapTypeId.ROADMAP
+ };
+
+ var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+
+ var markerOptions = {
+  position: new google.maps.LatLng(33.75428, -84.39016),
   map: map
-});
+ };
+ var marker = new google.maps.Marker(markerOptions);
+ 	marker.setMap(map);
 
-google.maps.event.addListener(autocomplete, 'place_changed', function() {
-  infoWindow.close();
-  var place = autocomplete.getPlace();
-  if (place.geometry.viewport) {
-    map.fitBounds(place.geometry.viewport);
-  } else {
-    map.setCenter(place.geometry.location);
-    map.setZoom(17);
-  }
-  marker.setPosition(place.geometry.location);
-  infoWindow.setContent('<div><strong>' + place.name + '</strong><br>');
+ var infoWindowOptions = {
+  content: 'Andrew Young School of Policy Studies'
+ };
+
+ var infoWindow = new google.maps.InfoWindow(infoWindowOptions);
+ google.maps.event.addListener(marker,'click',function(e){
+
   infoWindow.open(map, marker);
-  google.maps.event.addListener(marker,'click',function(e){
 
-    infoWindow.open(map, marker);
-
-  });
 });
-
-	// for set markers and points
-/*};
-var map = new google.maps.Map(document.getElementById('map'), mapOptions);
-
-var markerOptions = {
-    position: new google.maps.LatLng(33.75428, -84.39016),
-    map: map
-};
-var marker = new google.maps.Marker(markerOptions);
-marker.setMap(map);
-
-var infoWindowOptions = {
-    content: 'Andrew Young School of Policy Studies'
-};
-
-var infoWindow = new google.maps.InfoWindow(infoWindowOptions);
-google.maps.event.addListener(marker,'click',function(e){
-  
-  infoWindow.open(map, marker);
-  
-});*/
+}
+google.maps.event.addDomListener(window, 'load', initialize);
